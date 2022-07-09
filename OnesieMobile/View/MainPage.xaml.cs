ef62bfe228058b5fbc182b853f2dd0532e9141f8
otium99 <cosmicself@outlook.com>
2022-07-09 17:41:46 -0300

    list.Where(l => !l.Deleted).ToList() crashes IOS

--- OnesieMobile/View/MainPage.xaml.cs
+++ OnesieMobile/View/MainPage.xaml.cs
@@ -13,5 +13,18 @@ public partial class MainPage : ContentPage
 
     }
 
+	List<ActivityEntity> list = new() {
+		new ActivityEntity() { ActivityID = "123", Deleted = true },
+		new ActivityEntity() { ActivityID = "567", Deleted = false} };
+
+	private void CrashButton_Pressed(object sender, EventArgs e)
+	{
+		List<ActivityEntity> listLocal = list.Where(l => !l.Deleted).ToList();
+    }
+
+	private void NoCrashButton_Pressed(object sender, EventArgs e)
+	{
+        List<ActivityEntity> listLocal = list.Where(l => l.Deleted == false).ToList();
+    }
 }
 
